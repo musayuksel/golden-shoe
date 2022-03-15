0f7a64e12bdf70698d61568b7dfb515a42d7ff12
musayuksel <musayuxel@gmail.com>
2022-03-15 19:58:27 +0000

    feat:Add order for ids

--- server/api.js
+++ server/api.js
@@ -7,7 +7,7 @@ router.get("/", (_, res) => {
 });
 router.get("/all", (_, res) => {
 	const selectAllShoesQuery =
-				"SELECT shoes.id as shoesId,productName,category,imgLinks,productUserType,price,explanation,colour,amount,size from shoes INNER JOIN stock AS s ON s.shoeId = shoes.id;";
+				"SELECT shoes.id as shoesId,productName,category,imgLinks,productUserType,price,explanation,colour,amount,size from shoes INNER JOIN stock AS s ON s.shoeId = shoes.id ORDER BY shoesId;";
 			pool.query(selectAllShoesQuery, [	],(error,result)=>{
 					if (error) {
 						return res.status(500).send({ msg: "Database ERROR!!!" });
