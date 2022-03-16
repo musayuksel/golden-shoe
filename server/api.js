f93a8b8b7cbe0c29bbbfa78ee7e2ca2820e4dbbe
musayuksel <musayuxel@gmail.com>
2022-03-16 00:36:53 +0000

    feat:format json file for each shoe

--- server/api.js
+++ server/api.js
@@ -7,14 +7,42 @@ router.get("/", (_, res) => {
 });
 router.get("/all", (_, res) => {
 	const selectAllShoesQuery =
-				"SELECT shoes.id as shoesId,productName,category,imgLinks,productUserType,price,explanation,colour,amount,size from shoes INNER JOIN stock AS s ON s.shoeId = shoes.id ORDER BY shoesId;";
-			pool.query(selectAllShoesQuery, [	],(error,result)=>{
-					if (error) {
-						return res.status(500).send({ msg: "Database ERROR!!!" });
-					}
-					res.send(result.rows)
-				})
-		
+		"SELECT shoes.id as shoesId,productName,category,imgLinks,productUserType,price,explanation,colour,amount,size from shoes INNER JOIN stock AS s ON s.shoeId = shoes.id ORDER BY shoesId;";
+	pool.query(selectAllShoesQuery, [], (error, result) => {
+		if (error) {
+			return res.status(500).send({ msg: "Database ERROR!!!" });
+		}
+		//for each shoe add stock key and assign array of object [{color:"white", size:"6.5",amoung:5},{color:"white", size:"7",amoung:2}]
+		const shoesGroup = result.rows.reduce((eachShoes, currentShoe) => {
+			if (eachShoes[currentShoe.shoesid]) {
+				eachShoes[currentShoe.shoesid].stock.push({
+					colour: currentShoe.colour,
+					size: currentShoe.size,
+					amount: currentShoe.amount,
+				});
+			} else {
+				eachShoes[currentShoe.shoesid] = {
+					shoesId: currentShoe.shoesid,
+					productName: currentShoe.productname,
+					category: currentShoe.category,
+					productUserType: currentShoe.productusertype,
+					price: currentShoe.price,
+					explanation: currentShoe.explanation,
+					imgLink: currentShoe.imglinks,
+					stock: [
+						{
+							colour: currentShoe.colour,
+							size: currentShoe.size,
+							amount: currentShoe.amount,
+						},
+					],
+				};
+			}
+			return eachShoes;
+		}, {});
+
+		res.send(shoesGroup);
+	});
 });
 
 export default router;
