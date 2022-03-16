6ac0f32574c39c8c1738f28e77dd9860fd96f79e
musayuksel <musayuxel@gmail.com>
2022-03-16 10:28:35 +0000

    feat:import routes to AppJs

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-
+import "./styles/Main.css";
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
+	<>
+		<Navbar />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/men" element={<About />} />
+			<Route path="/women" element={<About />} />
+			<Route path="/kids" element={<About />} />
+		</Routes>
+	</>
 );
 
 export default App;
