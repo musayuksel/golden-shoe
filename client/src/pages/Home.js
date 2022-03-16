6e6269669ad6d9a4cac8caae908dd2e476d0b1f1
musayuksel <musayuxel@gmail.com>
2022-03-16 16:45:09 +0000

    Feat:Add shoe card fixes #7

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import fetchData from "../utils/fetchData";
 import "../styles/Home.css";
 import logo from "./logo.svg";
+import ShoeCard from "../components/ShoeCard";
 
 export function Home() {
 	const [allShoes, setAllShoes] = useState([]);
@@ -16,8 +17,14 @@ export function Home() {
 			console.log(error);
 		}
 	}, []);
-
-	return <main role="main">MAIN PAGE</main>;
+	function shoes(shoeArray) {
+		return shoeArray.map((shoe) => <ShoeCard key={shoe.shoesId} shoe={shoe} />);
+	}
+	return (
+		<main role="main">
+			<ul>{shoes(allShoes)}</ul>
+		</main>
+	);
 }
 
 export default Home;
