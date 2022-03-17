d348f2b915918a6e6623ab26967f53f21614a42c
musayuksel <musayuxel@gmail.com>
2022-03-17 13:24:43 +0000

    feat:Add fetch function and shoe state

--- client/src/components/Description.js
+++ client/src/components/Description.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import getAndUpdateState from "../utils/getAndUpdateState";
 
 export default function Description() {
 	const { shoesId } = useParams();
+	const [shoe, setShoe] = useState([]);
+	//get data from db and update state
+	useEffect(() => getAndUpdateState(`/shoe/${shoesId}`, setShoe), []);
+
+	console.log(shoe);
 	return (
-		<div>
+		<section className="description-container">
+			<img
+				className="description-img"
+				src="https://api.lorem.space/image/shoes?w=250&h=250"
+				alt="img"
+			/>
 			<h3>{shoesId}</h3>
 			<h2>Description PAGE WILL GO HERE</h2>
-		</div>
+		</section>
 	);
 }
