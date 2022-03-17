aa8d71d637a571696ac313b92ec97555f3dd58a3
musayuksel <musayuxel@gmail.com>
2022-03-17 09:16:09 +0000

    feat: Add categorie component fix #6

---
+++ client/src/components/Categories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/Categories.css";
+import { MdOutlineFilterList } from "react-icons/md";
+
+export default function Categories() {
+	const categoryArr = [
+		"All",
+		"Sale",
+		"Trainers",
+		"Running",
+		"Gym & Training",
+		"Football",
+		"Hiking & Outdoor",
+		"Walking",
+		"Basketball",
+		"Tennis",
+	];
+	const categoryList = categoryArr.map((category) => (
+		<li className="category-item" key={category}>
+			{category}
+		</li>
+	));
+	return (
+		<section className="category-main-container">
+			<ul className="category-list-container">{categoryList}</ul>
+			<button>
+				<span>Filter</span> <MdOutlineFilterList />
+			</button>
+		</section>
+	);
+}
