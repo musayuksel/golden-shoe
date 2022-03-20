c6357326a0c63fac16f0ac713d4b54e066b48dda
musayuksel <musayuxel@gmail.com>
2022-03-20 00:39:40 +0000

    Feat:Add end points to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
 import Description from "./components/Description";
+import ErrorPage from "./components/ErrorPage";
 import Navbar from "./components/Navbar";
+import Success from "./components/Success";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -16,6 +18,8 @@ const App = () => (
 			<Route path="/kids" element={<Home />} />
 			<Route path="/shoe/:shoesId" element={<Description />} />
 			<Route path="/cart" element={<Cart />} />
+			<Route path="/success" element={<Success />} />
+			<Route path="*" element={<ErrorPage />} />
 		</Routes>
 	</>
 );
