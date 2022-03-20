08f816a605351af39a0ed8fbc56487c49de317fd
musayuksel <musayuxel@gmail.com>
2022-03-20 00:38:07 +0000

    Feat:Add functions to the Cart Page

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import "../styles/Cart.css";
+import updateStockAmount from "../utils/updateStockAmount";
 import CartItem from "./CartItem";
+import LoadingAnimation from "./LoadingAnimation";
 
 export default function Cart() {
 	const [cartItems, setCartItems] = useState([]);
-
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	console.log({ cartItems });
 	useEffect(() => {
 		const cartItemsLocalStorage = JSON.parse(
 			localStorage.getItem("gs-cart") || "[]"
@@ -20,40 +24,51 @@ export default function Cart() {
 	const allItems = cartItems.map((item) => (
 		<CartItem key={item.localId} item={item} />
 	));
+
+	const navigate = useNavigate();
+	function handleCheckout() {
+		setIsSubmitting(true);
+		updateStockAmount(setIsSubmitting, navigate, setCartItems);
+	}
+
 	return (
-		<section className="cart-page">
-			<article className="cart-header">
-				<h2>Your Bag</h2>
-				<p>
-					Items in your bag are not reserved – check out now to make them yours.
-				</p>
-			</article>
-			<article className="cart-items-container">{allItems}</article>
-			<article className="cart-summary">
-				<p>Order Summary</p>
-				<div className="amount">
-					<p>3 items</p>
-					<p>£100</p>
-				</div>
-				<div className="delivery">
-					<p>Delivery</p>
-					<p>FREE</p>
-				</div>
-				<div className="total">
-					<p>TOTAL</p>
-					<p>£300</p>
-				</div>
-				<div className="vouchers">
-					<input
-						type="text"
-						name=""
-						id=""
-						placeholder="Enter your vouchers code"
-					/>
-					<button>Add</button>
-				</div>
-			</article>
-			<button onClick={() => setCartItems([])}>CHECKOUT</button>
-		</section>
+		<>
+			<LoadingAnimation isLoading={isSubmitting} />
+			<section className="cart-page">
+				<article className="cart-header">
+					<h2>Your Bag</h2>
+					<p>
+						Items in your bag are not reserved – check out now to make them
+						yours.
+					</p>
+				</article>
+				<article className="cart-items-container">{allItems}</article>
+				<article className="cart-summary">
+					<p>Order Summary</p>
+					<div className="amount">
+						<p>3 items</p>
+						<p>£100</p>
+					</div>
+					<div className="cart-summary-delivery">
+						<p>Delivery</p>
+						<p>FREE</p>
+					</div>
+					<div className="cart-summary-total">
+						<p>TOTAL</p>
+						<p>£300</p>
+					</div>
+					<div className="cart-summary-vouchers">
+						<input
+							type="text"
+							name=""
+							id=""
+							placeholder="Enter your vouchers code"
+						/>
+						<button>Add</button>
+					</div>
+				</article>
+				<button onClick={() => handleCheckout()}>CHECKOUT</button>
+			</section>
+		</>
 	);
 }
