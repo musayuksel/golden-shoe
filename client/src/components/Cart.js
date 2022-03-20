b98a2c9e840e69037c9909d1906a53845bd41842
musayuksel <musayuxel@gmail.com>
2022-03-20 21:52:39 +0000

    Feat:Add updateVoucher function fix #22

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -4,12 +4,16 @@ import "../styles/Cart.css";
 import updateStockAmount from "../utils/updateStockAmount";
 import CartItem from "./CartItem";
 import LoadingAnimation from "./LoadingAnimation";
+import { applyVoucher, findTotalPrice } from "../utils/cartHelperFucntions";
 
 export default function Cart() {
 	const [cartItems, setCartItems] = useState([]);
 	const [isSubmitting, setIsSubmitting] = useState(false);
 	const [isAmountChange, setIsAmountChange] = useState(false);
 	const [voucherAmount, setVoucherAmount] = useState(0);
+	const [voucherCode, setVoucherCode] = useState("");
+
+	// if amount of item change, update localstorage
 	useEffect(() => {
 		const cartItemsLocalStorage = JSON.parse(
 			localStorage.getItem("gs-cart") || "[]"
@@ -17,18 +21,34 @@ export default function Cart() {
 		setCartItems(cartItemsLocalStorage);
 	}, [isAmountChange]);
 
-	const totalPrice = cartItems.reduce(
-		(total, curr) => total + +curr.price * curr.choosedAmount,
-		0
-	);
+	// if item of cart change, update localstorage
 	useEffect(
 		() => localStorage.setItem("gs-cart", JSON.stringify(cartItems)),
 		[cartItems]
 	);
+	// if amount of item change, update Total price
+	const totalPrice = findTotalPrice(cartItems);
+
+	// if any item deleted from cart update state
 	function deleteItemFromCart(localId) {
 		const itemDeletedCart = cartItems.filter((item) => item.localId != localId);
 		setCartItems(itemDeletedCart);
 	}
+
+	const navigate = useNavigate();
+	function handleCheckout() {
+		setIsSubmitting(true);
+		updateStockAmount(setIsSubmitting, navigate, setCartItems);
+	}
+	async function handleVoucher(event) {
+		event.preventDefault();
+		const voucher = await applyVoucher(voucherCode);
+		if (voucher > 0) {
+			setVoucherAmount(voucher);
+			setVoucherCode("");
+		}
+	}
+
 	const allItems = cartItems.map((item) => (
 		<CartItem
 			key={item.localId}
@@ -37,13 +57,6 @@ export default function Cart() {
 			setIsAmountChange={setIsAmountChange}
 		/>
 	));
-
-	const navigate = useNavigate();
-	function handleCheckout() {
-		setIsSubmitting(true);
-		updateStockAmount(setIsSubmitting, navigate, setCartItems);
-	}
-
 	return (
 		<>
 			<LoadingAnimation isLoading={isSubmitting} />
@@ -71,13 +84,17 @@ export default function Cart() {
 						<p>{`£${Math.round(totalPrice * 100) / 100 - voucherAmount}`}</p>
 					</div>
 					<div className="cart-summary-vouchers">
-						<input
-							type="text"
-							name=""
-							id=""
-							placeholder="Enter your vouchers code"
-						/>
-						<button>Add</button>
+						<form onSubmit={handleVoucher}>
+							<input
+								type="text"
+								value={voucherCode}
+								placeholder="Enter your vouchers code"
+								onChange={(e) =>
+									setVoucherCode(e.target.value.toUpperCase().trim())
+								}
+							/>
+							<button type="submit">Add Voucher</button>
+						</form>
 					</div>
 				</article>
 				<button onClick={() => handleCheckout()}>CHECKOUT</button>
