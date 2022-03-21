a4e17d4d1bef695f06fb215207d3b506cebdfc4c
musayuksel <musayuxel@gmail.com>
2022-03-21 15:11:48 +0000

    Add test img URL

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,12 +7,29 @@ export function Home() {
 	const [allShoes, setAllShoes] = useState([]);
 	//get data from db and update state
 	useEffect(async () => getAndUpdateState("/all", setAllShoes), []);
+	const [imgURL, setImgURL] = useState("");
+	async function musa() {
+		const response = await fetch("http://localhost:3100/1.png");
+		console.log(response);
+		const image = await response.blob();
+		// Create a local URL of that image
+		const localUrl = URL.createObjectURL(image);
+		setImgURL(localUrl);
+	}
+	useEffect(() => {
+		musa();
+	}, []);
 
 	function shoes(shoeArray) {
 		return shoeArray.map((shoe) => <ShoeCard key={shoe.shoesId} shoe={shoe} />);
 	}
 	return (
 		<main role="main">
+			<img
+				style={{ width: "100px", height: "100px", border: "2px solid red" }}
+				src={imgURL}
+				alt="TEST"
+			/>
 			<Categories />
 			<ul className="shoe-cards-container">{shoes(allShoes)}</ul>
 		</main>
