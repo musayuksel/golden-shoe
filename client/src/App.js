7f96c3173309507c525886e9d3db915fec60164c
musayuksel <musayuxel@gmail.com>
2022-03-22 05:37:25 +0000

    Feat:Add searchkey state to parent

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
 import Description from "./components/Description";
+import React, { useState } from "react";
 import ErrorPage from "./components/ErrorPage";
 import Navbar from "./components/Navbar";
 import Success from "./components/Success";
@@ -9,65 +10,72 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import "./styles/Main.css";
 import Return from "./components/Return";
-const App = () => (
-	<>
-		<Navbar />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/shoes/:userType" element={<Home />} />
-			<Route path="/shoe/:shoesId" element={<Description />} />
-			<Route path="/cart" element={<Cart />} />
-			<Route path="/success" element={<Success />} />
-			<Route path="/return" element={<Return />} />
-			<Route path="*" element={<ErrorPage />} />
-		</Routes>
-		<footer>
-			<ul className="footer-items-container">
-				<li className="footer-item">
-					<Link to={"/"}>Delivery</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/"}>FAQ</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/return"}>Returns & Refunds</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/"}>Order Tracker</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/"}>Help</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/"}>Store Finder</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/"}>Sitemap</Link>{" "}
-				</li>
-				<li className="footer-item">
-					<Link to={"/"}>Payment</Link>{" "}
-				</li>
-			</ul>
+const App = () => {
+	const [searchKey, setSearchKey] = useState("");
+	console.log({ searchKey });
+	return (
+		<>
+			<Navbar setSearchKey={setSearchKey} />
+			<Routes>
+				<Route path="/" element={<Home searchKey={searchKey} />} />
+				<Route
+					path="/shoes/:userType"
+					element={<Home searchKey={searchKey} />}
+				/>
+				<Route path="/shoe/:shoesId" element={<Description />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/success" element={<Success />} />
+				<Route path="/return" element={<Return />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+			<footer>
+				<ul className="footer-items-container">
+					<li className="footer-item">
+						<Link to={"/"}>Delivery</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/"}>FAQ</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/return"}>Returns & Refunds</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/"}>Order Tracker</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/"}>Help</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/"}>Store Finder</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/"}>Sitemap</Link>{" "}
+					</li>
+					<li className="footer-item">
+						<Link to={"/"}>Payment</Link>{" "}
+					</li>
+				</ul>
 
-			<section>
-				<section className="social-links">
-					<a href="https://www.instagram.com/?hl=en" target={"_blank"}>
-						<BsInstagram />
-					</a>
-					<a href="https://en-gb.facebook.com/" target={"_blank"}>
-						<BsFacebook />
-					</a>
-					<a href="https://twitter.com/" target={"_blank"}>
-						<BsTwitter />
+				<section>
+					<section className="social-links">
+						<a href="https://www.instagram.com/?hl=en" target={"_blank"}>
+							<BsInstagram />
+						</a>
+						<a href="https://en-gb.facebook.com/" target={"_blank"}>
+							<BsFacebook />
+						</a>
+						<a href="https://twitter.com/" target={"_blank"}>
+							<BsTwitter />
+						</a>
+					</section>
+					<p>© Copyright 2022 Golden Shoe</p>
+					<a href="https://github.com/musayuksel/golden-shoe" target={"_blank"}>
+						Codebase
 					</a>
 				</section>
-				<p>© Copyright 2022 Golden Shoe</p>
-				<a href="https://github.com/musayuksel/golden-shoe" target={"_blank"}>
-					Codebase
-				</a>
-			</section>
-		</footer>
-	</>
-);
+			</footer>
+		</>
+	);
+};
 
 export default App;
