c42753794ff911640cbc5d75d6fa6f6acd45e41c
musayuksel <musayuxel@gmail.com>
2022-03-22 05:07:51 +0000

    Feat:Add error page and styles

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,5 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "../styles/Error.css";
 export default function ErrorPage() {
-	return <div>ErrorPage</div>;
+	return (
+		<section className="error-container">
+			<h1 className="title">404</h1>
+
+			<h4 className="sub-heading">Page Not Found</h4>
+
+			<p className="error-paragraph">
+				We're sorry, but the page you're looking for doesn't exist...
+			</p>
+
+			<Link className="go-home-page" to="/">
+				Go Back Home
+			</Link>
+		</section>
+	);
 }
