de7599f07cc02d6858b8b630b1398c3fff65bd80
musayuksel <musayuxel@gmail.com>
2022-03-22 03:22:01 +0000

    feat:Add success page

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,5 +1,22 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Success.css";
 export default function Success() {
-	return <div>Checkout Success Page </div>;
+	const navigate = useNavigate();
+	useEffect(() => {
+		setTimeout(() => {
+			navigate("/");
+		}, 5000);
+	}, []);
+
+	return (
+		<section className="success">
+			<h1>Checkout Success </h1>
+			<h2>Thank you for choosing us!!! </h2>
+			<p>You will redirect to the home page in 5 seconds...</p>
+			<button className="add-to-cart" onClick={() => navigate("/")}>
+				Return now
+			</button>
+		</section>
+	);
 }
