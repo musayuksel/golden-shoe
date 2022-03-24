0931547b7221ffebbd1f32630e34e305b230db2c
musayuksel <musayuxel@gmail.com>
2022-03-24 01:29:52 +0000

    feat: update clear interval function fix #47

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,14 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Success.css";
 export default function Success() {
+	let timer = 0;
 	const navigate = useNavigate();
 	useEffect(() => {
-		setTimeout(() => {
+		timer = setTimeout(() => {
 			navigate("/");
 		}, 10000);
+
+		return () => clearTimeout(timer);
 	}, []);
 
+	function handleReturnNow() {
+		clearTimeout(timer);
+		navigate("/");
+	}
 	const day = new Date();
 	let deliveryDay = day.setDate(day.getDate() + 7);
 	deliveryDay = day.toLocaleDateString("en");
@@ -17,9 +24,9 @@ export default function Success() {
 		<section className="success">
 			<h1>Checkout Success </h1>
 			<h2>Thank you for choosing us!!! </h2>
-			<h3>Our delivery dates will be {deliveryDay}</h3>
+			<h3>Your delivery dates will be {deliveryDay}</h3>
 			<p>You will redirect to the home page in 10 seconds...</p>
-			<button className="add-to-cart" onClick={() => navigate("/")}>
+			<button className="add-to-cart" onClick={handleReturnNow}>
 				Return now
 			</button>
 		</section>
