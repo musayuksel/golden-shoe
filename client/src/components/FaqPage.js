362001a16ef1641cc375d94ef2f0b84d4279de1f
musayuksel <musayuxel@gmail.com>
2022-03-24 17:02:12 +0000

    Feat: Add customer service info

--- client/src/components/FaqPage.js
+++ client/src/components/FaqPage.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { BsFillCaretUpFill, BsFillCaretDownFill } from "react-icons/bs";
+import {
+	BsFillCaretUpFill,
+	BsFillCaretDownFill,
+	BsPhoneFill,
+	BsWhatsapp,
+} from "react-icons/bs";
+import { FaFacebookMessenger } from "react-icons/fa";
 import "../styles/Faqpage.css";
 function QuestionCards({ question, answer }) {
 	// answers are placaholder for MVP
@@ -31,11 +37,50 @@ function QuestionCards({ question, answer }) {
 }
 export default function FaqPage() {
 	return (
-		<section className="faq-container">
-			<QuestionCards question="WHAT IS THE DELIVERY TIME & COST?" />
-			<QuestionCards question="HOW DO I RETURN MY PRODUCT(S)?" />
-			<QuestionCards question="CAN I EXCHANGE MY PRODUCT(S)?" />
-			<QuestionCards question="HOW DO I CHECK MY ORDER STATUS?" />
-		</section>
+		<>
+			<section className="faq-container">
+				<h1>FREQUENTLY ASKED QUESTIONS</h1>
+				<QuestionCards question="WHAT IS THE DELIVERY TIME & COST?" />
+				<QuestionCards question="HOW DO I RETURN MY PRODUCT(S)?" />
+				<QuestionCards question="CAN I EXCHANGE MY PRODUCT(S)?" />
+				<QuestionCards question="HOW DO I CHECK MY ORDER STATUS?" />
+			</section>
+			<section className="customer-service">
+				<h2>STILL CAN'T FIND YOUR ANSWER?</h2>
+				<p>ASK OUR CUSTOMER SERVICE</p>
+				<p>Mon - Fri : 9:00 AM to 5:00 PM </p>
+				<section className="contact-cards-container">
+					<article className="call-golden-shoe">
+						<div className="icon">
+							<BsPhoneFill />
+						</div>
+						<p>CALL</p>
+						<p>+44 1234 567 890 </p>
+						<p>
+							DEPENDING ON THE LANDLINE OR MOBILE NETWORK PROVIDER, ADDITIONAL
+							COSTS MAY APPLY.
+						</p>
+					</article>
+					<article className="whatsapp-golden-shoe">
+						<div className="icon">
+							<BsWhatsapp />
+						</div>
+						<p>ASK ADIDAS VIA WHATSAPP</p>
+						<p>+44 1234 567 890 </p>
+						<p>
+							Add number to the contact list on your smartphone and reach us
+							fast via WhatsApp.
+						</p>
+					</article>
+					<article className="facebook-golden-shoe">
+						<div className="icon">
+							<FaFacebookMessenger />
+						</div>
+						<p>ASK ADIDAS VIA FACEBOOK</p>
+						<p>Use the private message option to contact our support team.</p>
+					</article>
+				</section>
+			</section>
+		</>
 	);
 }
