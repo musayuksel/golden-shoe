6eb1ac49b5b90343b32895676383eaaaeac9ba63
musayuksel <musayuxel@gmail.com>
2022-03-24 16:43:05 +0000

    Refactor: Add reusable faq component

--- client/src/components/FaqPage.js
+++ client/src/components/FaqPage.js
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
 import { BsFillCaretUpFill, BsFillCaretDownFill } from "react-icons/bs";
 import "../styles/Faqpage.css";
-export default function FaqPage() {
+function QuestionCards({ question, answer }) {
+	// answers are placaholder for MVP
 	const [questionClicked, setQuestionClicked] = useState(false);
-
 	function handleQuestionClick(setState) {
 		setState((prev) => !prev);
 	}
 	return (
+		<article className="question">
+			<header>
+				<h4>{question}</h4>
+				<button
+					aria-label={questionClicked ? "hide answer" : "show answer"}
+					onClick={() => handleQuestionClick(setQuestionClicked)}
+				>
+					{questionClicked ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />}
+				</button>
+			</header>
+			{questionClicked && (
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, alias
+					expedita consequatur corrupti aperiam ad molestias corporis dolores
+					sit harum aut nemo sapiente, quasi eveniet a voluptas iusto quam
+					recusandae.
+				</p>
+			)}
+		</article>
+	);
+}
+export default function FaqPage() {
+	return (
 		<section className="faq-container">
-			<article className="question">
-				<header>
-					<h4>WHAT IS THE DELIVERY TIME & COST?</h4>
-					<button
-						aria-label={questionClicked ? "hide answer" : "show answer"}
-						onClick={() => handleQuestionClick(setQuestionClicked)}
-					>
-						{questionClicked ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />}
-					</button>
-				</header>
-				{questionClicked && (
-					<p>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod,
-						alias expedita consequatur corrupti aperiam ad molestias corporis
-						dolores sit harum aut nemo sapiente, quasi eveniet a voluptas iusto
-						quam recusandae.
-					</p>
-				)}
-			</article>
+			<QuestionCards question="WHAT IS THE DELIVERY TIME & COST?" />
+			<QuestionCards question="HOW DO I RETURN MY PRODUCT(S)?" />
+			<QuestionCards question="CAN I EXCHANGE MY PRODUCT(S)?" />
+			<QuestionCards question="HOW DO I CHECK MY ORDER STATUS?" />
 		</section>
 	);
 }
