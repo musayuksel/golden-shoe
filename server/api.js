d4878ea56959cb594a5bb434ec8db92383a43a64
musayuksel <musayuxel@gmail.com>
2023-07-09 14:42:36 +0100

    Add stock initial endpoint

--- server/api.js
+++ server/api.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import pool from "./db";
 import groupShoes from "./utils/groupShoes";
+import {
+	menShoesTableQuery,
+	womenShoesTableQuery,
+	kidsShoesTableQuery,
+	menStockTableQuery,
+	womenStockTableQuery,
+	kidStockTableQuery,
+} from "./utils/initialStockQueries";
 const router = Router();
 
 router.get("/", (_, res) => {
@@ -82,6 +90,77 @@ router.get("/stock", (req, res) => {
 			return res.status(500).send({ msg: "Database ERROR" });
 		}
 		res.send({ msg: "Database stock updated with new products." });
+	});
+});
+// This endpoint will fill out the DB initial stocks
+router.get("/stock-initial", (req, res) => {
+	// create men shoes table
+	const tableCreateQuery =
+		menShoesTableQuery + womenShoesTableQuery + kidsShoesTableQuery;
+	pool.query(tableCreateQuery, [], (error) => {
+		if (error) {
+			console.log(error);
+			return res.status(500).send({ msg: "Database ERROR" });
+		}
+	});
+	// initializate stock table for men shoes
+	const allShoesQuery =
+		"select * from shoes where shoes.productUserType='Men';";
+	pool.query(allShoesQuery, [], (error, result) => {
+		if (error) {
+			console.log(error);
+			return res.status(500).send({ msg: "Database ERROR" });
+		}
+		const allShoes = result.rows;
+
+		const insertStockQuery = allShoes.map(({ id }) => menStockTableQuery(id));
+		pool.query(insertStockQuery.join(""), [], (error) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).send({ msg: "Database ERROR" });
+			}
+			// res.send({ msg: "Database stock updated with new products." });
+		});
+	});
+
+	// initializate stock table for women shoes
+	const allWomenShoesQuery =
+		"select * from shoes where shoes.productUserType='Women';";
+	pool.query(allWomenShoesQuery, [], (error, result) => {
+		if (error) {
+			console.log(error);
+			return res.status(500).send({ msg: "Database ERROR" });
+		}
+		const allShoes = result.rows;
+
+		const insertStockQuery = allShoes.map(({ id }) => womenStockTableQuery(id));
+		pool.query(insertStockQuery.join(""), [], (error) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).send({ msg: "Database ERROR" });
+			}
+			// res.send({ msg: "Database stock updated with new products." });
+		});
+	});
+
+	// initializate stock table for women kids
+	const allKidShoesQuery =
+		"select * from shoes where shoes.productUserType='Kids';";
+	pool.query(allKidShoesQuery, [], (error, result) => {
+		if (error) {
+			console.log(error);
+			return res.status(500).send({ msg: "Database ERROR" });
+		}
+		const allShoes = result.rows;
+
+		const insertStockQuery = allShoes.map(({ id }) => kidStockTableQuery(id));
+		pool.query(insertStockQuery.join(""), [], (error) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).send({ msg: "Database ERROR" });
+			}
+			res.send({ msg: "Database stock updated with new products." });
+		});
 	});
 });
 
